501b8f8e5f6deac659202ae4327dac8018366978
backsteen <bram.elfvering@gmail.com>
2016-06-10 10:42:09 +0200

    Update omxplayer.py

--- Adafruit_Video_Looper/omxplayer.py
+++ Adafruit_Video_Looper/omxplayer.py
@@ -31,8 +31,8 @@ class OMXPlayer(object):
         """Play the provided movied file, optionally looping it repeatedly."""
         self.stop(3)  # Up to 3 second delay to let the old player stop.
         # Assemble list of arguments.
-        args = ['omxplayer --win 0 0 1024 600']
-        args.extend(['-o', self._sound])  # Add sound arguments.
+        args = ['omxplayer']
+-       args.extend(['-o', self._sound])  # Add sound arguments.
         args.extend(self._extra_args)     # Add extra arguments from config.
         if vol is not 0:
             args.extend(['--vol', str(vol)])
